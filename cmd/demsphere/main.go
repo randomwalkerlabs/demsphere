6a8680cd2f133aab9049cd9e420ed0ae02e7d04b
Nakul Bende <46679530+randomwalkerlabs@users.noreply.github.com>
2024-04-19 22:04:53 -0400

    Adding a second triangulator for stl with 'flipped' normal
    Specifically for making a 'depth' profile - this can be the inner shell that can be booleaned out of the outer shell - for a shell that can be lot form inside

--- cmd/demsphere/main.go
+++ cmd/demsphere/main.go
@@ -5,9 +5,10 @@ import (
 	"log"
 	"time"
 
+	"github.com/disintegration/imaging"
 	"github.com/fogleman/demsphere"
 	"github.com/fogleman/fauxgl"
-	kingpin "gopkg.in/alecthomas/kingpin.v2"
+	kingpin "github.com/alecthomas/kingpin/v2"
 )
 
 var (
@@ -37,6 +38,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Image, minDetail, maxDetail, meanRadius, minElevation, maxElevation, tolerance, exaggeration, scale
+
 	// mercury
 	// triangulator := demsphere.NewTriangulator(
 	// 	im, 6, 12, 2439400, -10764, 8994, 50, 4, 1.0/2439400)
@@ -46,18 +49,35 @@ func main() {
 	// 	im, 6, 11, 1737400, -18257, 21563, 50, 3, 1.0/1737400)
 
 	// mars
+	//triangulator := demsphere.NewTriangulator(
+	//	im, 9, 12, 3396190, -8201, 21241, 50, 10, 1.0/3396190)
+
+	// earth
 	triangulator := demsphere.NewTriangulator(
-		im, 9, 12, 3396190, -8201, 21241, 50, 10, 1.0/3396190)
+	im, 9, 12, 6373934, -10900, 8849, 50, 15, 1.0/6373934)
 
 	// pluto
 	// triangulator := demsphere.NewTriangulator(
 	// 	im, 6, 12, 1188300, -4101, 6491, 50, 3, 1.0/1188300)
 
-	done = timed("generating mesh")
+	done = timed("generating positive mesh")
 	triangles := triangulator.Triangulate()
 	done()
 
 	fmt.Println(len(triangles))
+
+	im = imaging.Invert(im)
+	triangulator = demsphere.NewTriangulator(
+		im, 9, 12, 6373934, -10900, 8849, 50, 15, 1.0/6373934)
+
+	inner := triangulator.Triangulate()
+	for i, t := range inner {
+		inner[i] = demsphere.Triangle{t.C, t.B, t.A}
+	}
+	done = timed("generating negative mesh")
+	triangles = append(triangles, inner...)
+	fmt.Println(len(triangles))
+	done()
 
 	// inner := fauxgl.NewSphere(4)
 	// inner.Transform(fauxgl.Scale(fauxgl.V(0.85, 0.85, 0.85)))
@@ -85,3 +105,17 @@ func main() {
 // 10,20971520,0.07043426041304851
 // 11,83886080,0.036106462472517295
 // 12,335544320,0.018169800357500515
+
+
+
+
+// Planet	Radius (m)	Min Elevation (m)	Max Elevation (m)
+// Mercury	2,439,700	-5,380	4,250
+// Venus	6,051,800	-650	650
+// Earth	6,371,000	-10,900	8,850
+// Mars	3,389,500	-8,200	21,900
+// Jupiter	69,911,000	N/A	N/A
+// Saturn	58,232,000	N/A	N/A
+// Uranus	25,362,000	N/A	N/A
+// Neptune	24,622,000	N/A	N/A
+// Pluto	1,186,000	-5,900	5,900
