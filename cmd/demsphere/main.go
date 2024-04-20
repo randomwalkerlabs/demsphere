1724d0362983908d73d93da15060b73330bac951
Nakul Bende <46679530+randomwalkerlabs@users.noreply.github.com>
2024-04-19 23:34:03 -0400

    Chnages to make variable selection a cased structure

--- cmd/demsphere/main.go
+++ cmd/demsphere/main.go
@@ -8,25 +8,26 @@ import (
 	"github.com/disintegration/imaging"
 	"github.com/fogleman/demsphere"
 	"github.com/fogleman/fauxgl"
+	//"github.com/schollz/progressbar/v3"
 	kingpin "github.com/alecthomas/kingpin/v2"
 )
+
+// Define a struct to hold the variables for each celestial body
+type CelestialVariables struct {
+	MinDetail      int
+	MaxDetail      int
+	MeanRadius     float64
+	MinElevation   float64
+	MaxElevation   float64
+	Tolerance      float64
+	Exaggeration   float64
+	Scale          float64
+	InnerShellScale float64
+}
 
 var (
 	inputFile  = kingpin.Flag("input", "Input DEM image to process.").Required().Short('i').ExistingFile()
 	//outputFile = kingpin.Flag("output", "Output STL file to write.").Required().Short('o').String() // Currently not in use
-)
-
-var (
-	planet           = "earth" // Planet
-	minDetail        = 3      //
-	maxDetail        = 5
-	meanRadius       = 6373934.0        // Mean radius of the planet
-	minElevation     = -10900.0         // Highest point of planet
-	maxElevation     = 8849.0           // Lowest point of planet
-	tolerance        = 100.0             // Tolerance - accurate to this meters
-	exaggeration     = 5.0             // Vertical exaggeration
-	scale            = 1.0 / meanRadius // Final scaling
-	innerShellScale  = 1.0               // Scaling for inner shell, compared to outer one
 )
 
 func timed(name string) func() {
@@ -40,6 +41,109 @@ func timed(name string) func() {
 }
 
 func main() {
+	// Define variables for all celestial bodies using a map
+	celestialVariables := map[string]CelestialVariables{
+		"mercury": {
+			MinDetail:      15,
+			MaxDetail:      30,
+			MeanRadius:     2439700,
+			MinElevation:   -5000,
+			MaxElevation:   5500,
+			Tolerance:      50,
+			Exaggeration:   15,
+			Scale:          1 / 2439700.0,
+			InnerShellScale: 1,
+		},
+		"venus": {
+			MinDetail:      20,
+			MaxDetail:      35,
+			MeanRadius:     6051800,
+			MinElevation:   -11000,
+			MaxElevation:   13000,
+			Tolerance:      50,
+			Exaggeration:   20,
+			Scale:          1 / 6051800.0,
+			InnerShellScale: 1,
+		},
+		"earth": {
+			MinDetail:      15,
+			MaxDetail:      30,
+			MeanRadius:     6373934,
+			MinElevation:   -10900,
+			MaxElevation:   8849,
+			Tolerance:      50,
+			Exaggeration:   15,
+			Scale:          1 / 6373934.0,
+			InnerShellScale: 1,
+		},
+		"mars": {
+			MinDetail:      25,
+			MaxDetail:      40,
+			MeanRadius:     3389500,
+			MinElevation:   -8200,
+			MaxElevation:   21200,
+			Tolerance:      50,
+			Exaggeration:   25,
+			Scale:          1 / 3389500.0,
+			InnerShellScale: 1,
+		},
+		"jupiter": {
+			MinDetail:      30,
+			MaxDetail:      45,
+			MeanRadius:     69911000,
+			MinElevation:   0, // No solid surface
+			MaxElevation:   0, // No solid surface
+			Tolerance:      50,
+			Exaggeration:   30,
+			Scale:          1 / 69911000.0,
+			InnerShellScale: 1,
+		},
+		"saturn": {
+			MinDetail:      30,
+			MaxDetail:      45,
+			MeanRadius:     58232000,
+			MinElevation:   0, // No solid surface
+			MaxElevation:   0, // No solid surface
+			Tolerance:      50,
+			Exaggeration:   30,
+			Scale:          1 / 58232000.0,
+			InnerShellScale: 1,
+		},
+		"uranus": {
+			MinDetail:      30,
+			MaxDetail:      45,
+			MeanRadius:     25362000,
+			MinElevation:   0, // No solid surface
+			MaxElevation:   0, // No solid surface
+			Tolerance:      50,
+			Exaggeration:   30,
+			Scale:          1 / 25362000.0,
+			InnerShellScale: 1,
+		},
+		"neptune": {
+			MinDetail:      30,
+			MaxDetail:      45,
+			MeanRadius:     24622000,
+			MinElevation:   0, // No solid surface
+			MaxElevation:   0, // No solid surface
+			Tolerance:      50,
+			Exaggeration:   30,
+			Scale:          1 / 24622000.0,
+			InnerShellScale: 1,
+		},
+		"moon": {
+			MinDetail:      15,
+			MaxDetail:      30,
+			MeanRadius:     1737100,
+			MinElevation:   -9000,
+			MaxElevation:   10800,
+			Tolerance:      50,
+			Exaggeration:   15,
+			Scale:          1 / 1737100.0,
+			InnerShellScale: 1,
+		},
+	}
+
 	var done func()
 	kingpin.Parse()
 	// Reading input image
@@ -49,9 +153,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Choose planet
+	planet := "earth" // Change this according to user input
+
+	// Retrieve variables for the chosen planet
+	variables, ok := celestialVariables[planet]
+	if !ok {
+		log.Fatalf("planet %s not found in celestialVariables", planet)
+	}
 	// Make outer shell
 	triangulator := demsphere.NewTriangulator(
-	im, minDetail, maxDetail, meanRadius, minElevation, maxElevation, tolerance, exaggeration, scale)
+		im, variables.MinDetail, variables.MaxDetail, variables.MeanRadius, variables.MinElevation, variables.MaxElevation, variables.Tolerance, variables.Exaggeration, variables.Scale)
 	done = timed("generating outer mesh")
 	triangles := triangulator.Triangulate()
 	done()
@@ -59,7 +171,7 @@ func main() {
 	// Making inner shell
 	im = imaging.Invert(im) //Inverting image
 	triangulator = demsphere.NewTriangulator(
-	im, minDetail, maxDetail, meanRadius, minElevation, maxElevation, tolerance, exaggeration, scale*float64(innerShellScale))
+		im, variables.MinDetail, variables.MaxDetail, variables.MeanRadius, variables.MinElevation, variables.MaxElevation, variables.Tolerance, variables.Exaggeration, variables.Scale*float64(variables.InnerShellScale))
 	// Flippig normals
 	inner := triangulator.Triangulate()
 	for i, t := range inner {
@@ -72,7 +184,7 @@ func main() {
 	// Writing stl
 	done = timed("writing output")
 	// Make a filename with important parameters to test the output
-	filename := fmt.Sprintf("%s_%d_%d_%d_%d.stl", planet, minDetail, maxDetail, tolerance, exaggeration)
+	filename := fmt.Sprintf("%s_%d_%d_%d_%d_%d.stl", planet, variables.MinDetail, variables.MaxDetail, variables.Tolerance, variables.Exaggeration, variables.Scale)
 	demsphere.WriteSTLFile(filename, triangles)
 	done()
 }
